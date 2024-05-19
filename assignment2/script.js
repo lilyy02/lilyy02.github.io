2bd0b42e34375f737eb2c9b1fec930ebb2f1d304
lilyy02 <s3899541@student.rmit.edu.au>
2024-05-19 11:57:51 +1000

    icon and style changes

---
+++ assignment2/script.js
@@ -0,0 +1,23 @@
+const video = document.querySelector("#custom-video-player");
+const playPauseBtn = document.querySelector("#play-pause-btn");
+const playPauseImg = document.querySelector("#play-pause-img");
+const progressBar = document.querySelector("#progress-bar-fill");
+video.removeAttribute("controls");
+// playPauseBtn.addEventListener("click", togglePlayPause);
+video.addEventListener("timeupdate", updateProgressBar);
+function togglePlayPause() {
+  if (video.paused || video.ended) {
+    video.play();
+    // icon should change to pause when video is playing, the button should show the action that you will take
+    playPauseImg.src = "icons8-pause-90.png";
+  } else {
+    video.pause();
+    playPauseImg.src = "icons8-play-90.png";
+  }
+}
+function updateProgressBar() {
+  const value = (video.currentTime / video.duration) * 100;
+  progressBar.style.width = value + "%";
+}
+// Add other functionalities here
+// icons change to ghostwhite on hover?

5b17617bd23df46103b33d81967c2056d1b51426
lilyy02 <s3899541@student.rmit.edu.au>
2024-05-20 10:49:42 +1000

    css styling adjustments

--- assignment2/script.js
+++ assignment2/script.js
@@ -3,10 +3,12 @@ const video = document.querySelector("#custom-video-player");
 const playPauseBtn = document.querySelector("#play-pause-btn");
 const playPauseImg = document.querySelector("#play-pause-img");
 const progressBar = document.querySelector("#progress-bar-fill");
+// removing controls for the video only seems to work when placed before any functions
 video.removeAttribute("controls");
 // a lot of the javascript was done using https://www.geeksforgeeks.org/create-a-music-player-using-javascript/ as a guide for how to load multiple tracks and have the one set of controls work for all the songs
 // const: variable cannot be reassigned a new value 
 // let: variable can be reassigned new value
+// let has block scope. this means variables declared within {} can't be accessed from outside
 // variables need to be defined in html document, eg. there must be an element with "track-title" as the class
 // # before name: item only accessible to things inside the class. targets specific elements with a unique id.
 // "." targets multiple elements with a particular class
@@ -15,7 +17,7 @@ let title = document.querySelector("track-title");
 let artist = document.querySelector("track-artist");
 const skipBtn = document.querySelector("#skip-btn");
 const prevBtn = document.querySelector("prev-btn");
-let seekSlider = document.querySelector("");
+let seekSlider = document.querySelector("seek-slider");
 const volumeSlider = document.querySelector("");
 let time = document.querySelector("");
 let duration = document.querySelector("");
@@ -27,8 +29,8 @@ let currentTrack = document.createElement('audio');
 // the track list is an array to store multiple values in the one variable
 // each track is defined as an object in the track list - whats between each set of curly brackets counts as an object
 // data such as title, artist and path are all info associated with one object 
-// having the tracklist stored in an array in the javascript means that the information such as the song title and artist for the currently playing song can be updated to match the current media being played.
-const tracklist = [
+// having the tracklist stored in an array in the javascript lets me have multiple possible songs play with the button functions and dynamically update info such as the song title and artist.
+const playlist = [
   {
     title: "Underwater Exploration",
     artist: "Godmode",
@@ -71,12 +73,12 @@ const tracklist = [
   },
 ];
 
-
+// trackIndex records which song in the playlist is currently loaded. 0 = first track in playlist
 let trackIndex = 0;
 // isPlaying variable only needs to be a boolean, true or false. set to false by default, will start playing when isPlaying is set to true
+// when play button is clicked, isPlaying should be toggled between true/false
 let isPlaying = false;
 let updateTimer;
-
 
 
 
@@ -85,11 +87,11 @@ let updateTimer;
 function loadTrack(trackIndex) {
  clearInterval(updateTimer);
  time.textContent = "00:00";
- duration.textContent = "00:00"
+ duration.textContent = "00:00";
  seekSlider.value = 0;
 // .textcontent retrieves the text data associated with the current song
-title.textContent = tracklist[trackIndex].title;
-artist.textContent = tracklist[trackIndex].artist;
+title.textContent = playlist[trackIndex].title;
+artist.textContent = playlist[trackIndex].artist;
 // 1000ms delay prevents timer from updating too frequently to the point where there is lag
 updateTimer = setInterval(seekUpdate,1000)
 // listens for the event that the current track has ended, then plays the next track in the list
@@ -127,14 +129,20 @@ function pauseTrack () {
 }
 
 function skip () {
-  if (trackIndex < tracklist.length -1)
+  if (trackIndex < playlist.length -1)
     trackIndex +=1;
   else trackIndex = 0;
   loadTrack(trackIndex);
   playTrack();
 }
 
-function back () {}
+function back () {
+  if (trackIndex > 0)
+    trackIndex -=1;
+    else trackIndex = playlist.length -1;
+    loadtrack(trackIndex);
+    playTrack();
+}
 
 
 

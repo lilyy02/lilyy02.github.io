7ec2b9286cf5d6da48c4d2a97af9a56e6cce2d84
lilyy02 <s3899541@student.rmit.edu.au>
2024-05-20 15:53:26 +1000

    Chrome test push
     video, play/pause button not working again in preview. audio not playing

--- assignment2/script.js
+++ assignment2/script.js
@@ -2,7 +2,7 @@
 const video = document.querySelector("#custom-video-player");
 const playPauseBtn = document.querySelector("#play-pause-btn");
 const playPauseImg = document.querySelector("#play-pause-img");
-const progressBar = document.querySelector("#progress-bar-fill");
+// const progressBar = document.querySelector("#progress-bar-fill");
 // removing controls for the video only seems to work when placed before any functions
 video.removeAttribute("controls");
 // a lot of the javascript was done using https://www.geeksforgeeks.org/create-a-music-player-using-javascript/ as a guide for how to load multiple tracks and have the one set of controls work for all the songs
@@ -18,14 +18,15 @@ let artist = document.querySelector("track-artist");
 const skipBtn = document.querySelector("#skip-btn");
 const prevBtn = document.querySelector("prev-btn");
 let seekSlider = document.querySelector("seek-slider");
-const volumeSlider = document.querySelector("");
-let time = document.querySelector("");
-let duration = document.querySelector("");
+// const volumeSlider = document.querySelector("");
+// let time = document.querySelector("");
+// let duration = document.querySelector("");
 
 
 
 // audio has the let keyword as the file it is sourcing will change, while there is only one video present.
-let currentTrack = document.createElement('audio');
+let currentTrack = document.createElement("audio");
+// should create an audio object
 // the track list is an array to store multiple values in the one variable
 // each track is defined as an object in the track list - whats between each set of curly brackets counts as an object
 // data such as title, artist and path are all info associated with one object 
@@ -78,22 +79,23 @@ let trackIndex = 0;
 // isPlaying variable only needs to be a boolean, true or false. set to false by default, will start playing when isPlaying is set to true
 // when play button is clicked, isPlaying should be toggled between true/false
 let isPlaying = false;
-let updateTimer;
+// let updateTimer;
 
 
 
 // a function is used when the play button is clicked to load a track from the list.
 // loadtrack function clears the progress on the seek bar
 function loadTrack(trackIndex) {
- clearInterval(updateTimer);
- time.textContent = "00:00";
- duration.textContent = "00:00";
- seekSlider.value = 0;
+ // clearInterval(updateTimer);
+ // resetValues();
+ // should look to the correspending item in the array based on the current trackIndex, then use its src.
+ currentTrack.src = playlist[trackIndex].src;
+ currentTrack.load();
 // .textcontent retrieves the text data associated with the current song
 title.textContent = playlist[trackIndex].title;
 artist.textContent = playlist[trackIndex].artist;
 // 1000ms delay prevents timer from updating too frequently to the point where there is lag
-updateTimer = setInterval(seekUpdate,1000)
+// updateTimer = setInterval(seekUpdate,1000)
 // listens for the event that the current track has ended, then plays the next track in the list
 currentTrack.addEventListener("ended", nextTrack);
 }
@@ -106,7 +108,7 @@ currentTrack.addEventListener("ended", nextTrack);
 
 
 function togglePlayPause() {
-  if (video.paused || video.ended) {
+  if (isPlaying=false) {
     video.play();
     playPauseImg.src = "assets/icons/icons8-pause-90.png";
     playTrack();
@@ -119,6 +121,7 @@ function togglePlayPause() {
 // seem to have to put toggling video and audio on in seperate functions otherwise the video will get the default controls for some reason
 // playTrack function is seperated to prevent accidental looping
 function playTrack () {
+// currentTrack should 
   currentTrack.play();
   isPlaying = true;
 }
@@ -147,15 +150,15 @@ function back () {
 
 
 // Progress bar to be changed to show current time of song rather than video. May add seek function if I am able to.
-function updateProgressBar() {
-  const value = (audio.currentTime / audio.duration) * 100;
-  progressBar.style.width = value + "%";
-}
+// function updateProgressBar() {
+//   const value = (audio.currentTime / audio.duration) * 100;
+//   progressBar.style.width = value + "%";
+// }
 // function loop() {
 //   audio.loop();
 // }
 
-function togglemute() {}
+// function togglemute() {}
 
 // Add other functionalities here
 loadTrack(trackIndex);
